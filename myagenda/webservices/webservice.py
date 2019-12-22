8c7fb468b4d0a0f12f418b1f6afb0c2a294507d8
Anas Touda Lachiri <43256@etu.he2b.be>
2019-12-22 21:25:40 +0100

    web service with all event

--- myagenda/webservices/webservice.py
+++ myagenda/webservices/webservice.py
@@ -15,8 +15,25 @@ call = functools.partial(
     xmlrpc.client.ServerProxy(ROOT + 'object').execute,
     DB, uid, PASS)
 
-# 2. Read the sessions
-events = call('myagenda.event.student', 'search_read',
-              [], ['name', 'typeEvent'])
-for event in events:
-    print("Event %s (%s type)" % (event['name'], event['typeEvent']))
+# 2. Read the events student
+events_student = call('myagenda.event.student', 'search_read',
+                      [], ['name', 'typeEvent', 'attendees_ids'])
+for event in events_student:
+    if uid in event['attendees_ids']:
+        print("Event %s (%s)" % (event['name'], event['typeEvent']))
+
+# 2. Read the events pedagogic
+
+events_pedagogic = call('myagenda.event.pedagogic', 'search_read',
+                        [], ['name', 'typeEvent', 'attendees_ids'])
+for event in events_pedagogic:
+    if uid in event['attendees_ids']:
+        print("Event %s (%s type)" % (event['name'], event['typeEvent']))
+
+# 2. Read the events administrative
+
+events_administrative = call('myagenda.event.administrative', 'search_read',
+                             [], ['name', 'typeEvent', 'attendees_ids'])
+for event in events_administrative:
+    if uid in event['attendees_ids']:
+        print("Event %s (%s type)" % (event['name'], event['typeEvent']))
