66d0eb0881f79c49916c56afbdc39bf2d75be4fd
Anas Touda Lachiri <43256@etu.he2b.be>
2019-12-27 20:58:50 +0100

    Fixed security and relation agenda-partner-event

--- myagenda/partner.py
+++ myagenda/partner.py
@@ -9,10 +9,29 @@ class Partner(models.Model):
         default='attendee'
     )
 
-    events_ids = fields.Many2many('myagenda.event',
-                                  string="Events", readonly=True)
-    agenda_ids = fields.Many2many(
-        'myagenda.agenda',
-        string='Agenda',
+    events_student_ids = fields.Many2many('myagenda.event.student',
+                                          string="Events student", readonly=True)
+
+    events_pedagogic_ids = fields.Many2many('myagenda.event.pedagogic',
+                                            string="Events pedagogic", readonly=True)
+
+    events_administrative_ids = fields.Many2many('myagenda.event.administrative',
+                                                 string="Events administrative", readonly=True)
+
+    agenda_student_ids = fields.Many2many(
+        'myagenda.agenda.student',
+        string='Agenda student',
+        readonly=True
+    )
+
+    agenda_pedagogic_ids = fields.Many2many(
+        'myagenda.agenda.pedagogic',
+        string='Agenda pedagogic',
+        readonly=True
+    )
+
+    agenda_administrative_ids = fields.Many2many(
+        'myagenda.agenda.administrative',
+        string='Agenda administrative',
         readonly=True
     )
