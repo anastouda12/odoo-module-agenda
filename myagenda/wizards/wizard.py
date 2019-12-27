dca424407471caff9c9baba80f9836c8160f47b8
Anas Touda Lachiri <43256@etu.he2b.be>
2019-12-27 23:47:46 +0100

    add wizard for agenda

--- myagenda/wizards/wizard.py
+++ myagenda/wizards/wizard.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions, _
 
 
 class Wizard(models.TransientModel):
@@ -11,11 +11,20 @@ class Wizard(models.TransientModel):
                               readonly=True,
                               string='User'
                               )
+    event_id = fields.Many2one('myagenda.event',
+                               string="Event", required=True,
+                               readonly=True
+                               )
 
     @api.multi
     def subscribe(self):
-        self.event_id.attendees_ids |= self.env.user.partner_id
-        return {}
+        if self.env.user.partner_id in self.event_id.agenda_id.attendees_ids:
+            self.event_id.attendees_ids |= self.env.user.partner_id
+            return {}
+        else:
+            raise exceptions.ValidationError(
+                _("You are not registered in the corresponding agenda !)"))
+            return {}
 
     @api.multi
     def unsubscribe(self):
@@ -61,3 +70,75 @@ class WizardEventAdministrative(models.TransientModel):
                                string="Event", required=True, default=_default_event,
                                readonly=True
                                )
+
+
+class WizardAgendaStudent(models.TransientModel):
+    _name = 'myagenda.agenda.student.wizard'
+    _inherit = "myagenda.wizard"
+
+    def _default_event(self):
+        return self.env['myagenda.agenda.student'].browse(self._context.get('active_id'))
+
+    agenda_id = fields.Many2one('myagenda.agenda.student',
+                                string="Agenda", required=True, default=_default_event,
+                                readonly=True
+                                )
+
+    @api.multi
+    def subscribe(self):
+        self.agenda_id.attendees_ids |= self.env.user.partner_id
+        return {}
+
+    @api.multi
+    def unsubscribe(self):
+        partner = self.env.user.partner_id
+        self.agenda_id.attendees_ids = [(2, partner.id)]
+        return {}
+
+
+class WizardAgendaPedagogic(models.TransientModel):
+    _name = 'myagenda.agenda.pedagogic.wizard'
+    _inherit = "myagenda.wizard"
+
+    def _default_event(self):
+        return self.env['myagenda.agenda.pedagogic'].browse(self._context.get('active_id'))
+
+    agenda_id = fields.Many2one('myagenda.agenda.pedagogic',
+                                string="Agenda", required=True, default=_default_event,
+                                readonly=True
+                                )
+
+    @api.multi
+    def subscribe(self):
+        self.agenda_id.attendees_ids |= self.env.user.partner_id
+        return {}
+
+    @api.multi
+    def unsubscribe(self):
+        partner = self.env.user.partner_id
+        self.agenda_id.attendees_ids = [(2, partner.id)]
+        return {}
+
+
+class WizardAgendaPedagogic(models.TransientModel):
+    _name = 'myagenda.agenda.administrative.wizard'
+    _inherit = "myagenda.wizard"
+
+    def _default_event(self):
+        return self.env['myagenda.agenda.administrative'].browse(self._context.get('active_id'))
+
+    agenda_id = fields.Many2one('myagenda.agenda.administrative',
+                                string="Agenda", required=True, default=_default_event,
+                                readonly=True
+                                )
+
+    @api.multi
+    def subscribe(self):
+        self.agenda_id.attendees_ids |= self.env.user.partner_id
+        return {}
+
+    @api.multi
+    def unsubscribe(self):
+        partner = self.env.user.partner_id
+        self.agenda_id.attendees_ids = [(2, partner.id)]
+        return {}
